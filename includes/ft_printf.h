059bec5036e6f3f794c073e404df6c31cadc9f2b
dmahoro <dmahoro-@c1r8p3.42nice.fr>
2021-04-20 08:41:40 +0200

    new to keep working home

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,13 +6,16 @@
 /*   By: dmahoro- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 20:01:42 by dmahoro-          #+#    #+#             */
-/*   Updated: 2021/04/18 17:09:49 by dmahoro-         ###   ########.fr       */
+/*   Updated: 2021/04/19 23:28:59 by dmahoro-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef	FT_PRINTF_H
 # define FT_PRINTF_H
 
-int		ft_printf(char const *indata, ...);
+# include <stdarg.h>
+
+int		ft_printf(char const *format, ...);
+int		ft_vdprintf(int fd, const char *format, va_list ap);
 
 #endif
